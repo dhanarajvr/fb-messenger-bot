9240135243dd6bcd24d22072a208dd56260c3efb
dhanraj <dhanarajvr@gmail.com>
2017-04-13 14:02:37 +0530

    adding ccdet

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's face-book ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-			'''		    
+		    '''		    
 			if len(message_text.split())>2:
 			text = message_text
 			matches = tool.check(text)
